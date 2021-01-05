7ead842f5442fae19ed15f7b8291e56579b209ea
Aaron Bonetti <aaronmikebonetti@gmail.com>
2021-01-04 20:16:49 -0700

    2 | created search from ticker JSON

--- src/app/stocks/stocks.component.ts
+++ src/app/stocks/stocks.component.ts
@@ -1,7 +1,7 @@
 import { StocksUiService } from './../stocks-ui.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Ticker } from '../interfaces/ticker.model';
+import * as ticker from '../../assets/tickers/tickers.json';
 
 
 @Component({
@@ -11,14 +11,30 @@ import { Ticker } from '../interfaces/ticker.model';
 })
 
 export class StocksComponent implements OnInit {
-
-  // getTickers$: Observable<[Tickers]> = this.uiService.getStocks$.asObservable();
+  tickerString: string;
+  tickerParsed: [Ticker];
 
   constructor(
     private uiService: StocksUiService
   ) { }
 
   ngOnInit(): void {
+    this.tickerString = JSON.stringify(ticker).toLowerCase()
+    this.tickerParsed = JSON.parse(this.tickerString).default
+  // use css to capitalize the name see scss
+    this.tickerParsed.map(ticker=>{
+      console.log(ticker.symbol.toLocaleUpperCase(), ticker.name)
+    })
+
+  }
+
+  onKey(e){
+    // document.querySelector(selector).addEventListener('keypress', function() {
+    //   if (e.which <= 90 && e.which >= 48 || e.which >= 96 && e.which <= 105) {
+    //     alert('keycode ' + e.which + '  triggered this event');
+    //     //do whatever
+      // }
+    // });
   }
 
 }
